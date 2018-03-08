41b959340a34e9e7a0898c1b336201de69c2be09
Evan <burbidge@tcd.ie>
2018-03-08 19:25:29 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,42 @@
+# Rabbit MQ Interface
+When working on a project I found that I had need of using Rabbit MQ between a number of difference microservices.
+In order to get this functionality I introduced RabbitMQ to my project. What started out as a small function became this class and now it's free
+for anyone who wishes to use it. 
+
+# Installation
+- Installation via npm `npm install rabbit-mq-interface`
+- Feel free to fork this project and use it.
+
+# Api
+## Creating a connection
+
+```typescript 
+    import { RabbitMqInterface } from 'rabbit-mq-interface'; 
+    const myConnection = new RabbitMqInterface(
+            `myQueue`, 
+            `amqp://localhost` 
+             queueProcessingFunction(),
+    );
+``` 
+
+## Sending a message
+
+```typescript
+    myConnection.publish({ myTest: 'testing data' });
+```
+
+## Serializing a message
+Every message is sent in a Buffer data type. This will allow us to turn it into an object
+```typescript
+  const decoded = myConnection.decode(message.content);
+```
+
+
+## Copyright
+Copyright 2018 Evan Burbidge
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/rabbit.client.ts
@@ -0,0 +1,163 @@
+import * as amqp from 'amqplib/callback_api';
+/**
+ * @class
+ * @name RabbigMqInterface
+ * @description
+ *  This class provides us a number of methods for dealing with connecting to
+ *  amqplib and allows us to publish and send events to rabbit mq and digest
+ *  those events
+ * */
+class RabbitMqInterface {
+
+  private queue: string;
+  private connection: any;
+  private pubChannel: any;
+  private consumerHandler: any;
+  private offlinePubQueue: any[] = [];
+  private exchangeName: string = ``;
+
+  constructor(
+    private queueName: string,
+    private connectionUri: string,
+    consumerHandler: Function = msg => console.log(msg.content.toString())
+  ) {
+    this.queue = this.queueName;
+    this.consumerHandler = consumerHandler;
+    this.startRabbit();
+  }
+
+  /**
+   * @method
+   * @description
+   * this method is called once upon start up
+   * and the recursively anytime we have an error
+   * */
+  startRabbit() {
+    amqp.connect(
+      this.connectionUri,
+      (err, conn) => {
+        if (err) {
+          return this.handleRabbitClose();
+        }
+        this.connection = conn;
+        this.connection.on('error', err => this.handleRabbitErrror(err));
+        this.connection.on('close', () => this.handleRabbitClose());
+        console.log(`[AMQP] connection established`, this.queue);
+        this.startPublisher();
+        this.startConsumer();
+      },
+    );
+  }
+
+  /**
+   * @method
+   * @name handleRabbitErrror
+   * @description
+   *  This will handleRabbitErrror
+   * */
+  handleRabbitErrror(err) {
+    if (err.message !== 'Connection closing') {
+      console.error('[AMQP] conn error', err.message);
+    }
+  }
+
+  /**
+   * @method
+   * @name handleRabbitClose
+   * @description
+   *  This will handleRabbitClose
+   * */
+  handleRabbitClose() {
+    console.log(`[AMQP] Restarting`);
+    return setTimeout(() => this.startRabbit(), 1000);
+  }
+
+  /**
+   * @method
+   * @description
+   *  This will start our publications channel for
+   *  rabbit mq
+   * */
+  startPublisher() {
+    this.connection.createConfirmChannel((err, ch) => {
+      if (this.closeOnError(err)) return;
+      this.pubChannel = ch;
+      this.pubChannel.on('error', err => console.log(err));
+      this.pubChannel.on('close', () => console.log(`CHANNEL IS CLOSING`));
+      while (true) {
+        const m = this.offlinePubQueue.shift();
+        if (!m) break;
+        this.publish(m[2]);
+      }
+    });
+  }
+
+  /**
+   * @method
+   * @description
+   *  This is our main publishing function
+   *  it will allow us to publish methods
+   * @param {Object} content
+   * */
+  publish(content) {
+    try {
+      this.pubChannel.publish(
+        this.exchangeName,
+        this.queueName,
+        new Buffer(JSON.stringify(content)),
+        (err) => {
+          if (err) {
+            console.log(`[AMQP] publish`, err);
+            this.offlinePubQueue.push([this.exchangeName, this.queueName, content]);
+            this.pubChannel.connection.close();
+          }
+          console.log(`[AMQP] published`);
+        },
+      );
+    } catch (e) {
+      console.log(`[AMQP] publish`, e.message);
+      this.offlinePubQueue.push([this.exchangeName, this.queueName, content]);
+    }
+  }
+
+  /**
+   * @method
+   * @description
+   *  this will start a consumer that will ack a message if processed
+   * */
+  startConsumer() {
+    this.connection.createChannel((err, ch) => {
+      if (this.closeOnError(err)) return;
+      this.pubChannel = ch;
+      this.pubChannel.on('error', err => console.error('[AMQP] channel error', err.message));
+      this.pubChannel.on('close', () => console.log('[AMQP] channel closed'));
+      this.pubChannel.prefetch(10);
+      this.pubChannel.assertQueue(this.queue, { durable: true }, (err) => {
+        if (this.closeOnError(err)) return;
+        this.pubChannel.consume(
+          this.queue,
+          this.consumerHandler(this.pubChannel),
+          { noAck: false },
+        );
+        console.log('[AMQP] Worker is started');
+      });
+    });
+  }
+
+  /**
+   * @method
+   * @description
+   *  if there is a major error we're going to close the main
+   *  connection to rabbit
+   * */
+  closeOnError(err) {
+    if (!err) return err;
+    console.log(`[FATAL AMQP ERROR CLOSING]`, err);
+    this.connection.close();
+    return true;
+  }
+}
+
+module.exports = {
+  RabbitMqInterface: RabbitMqInterface
+};
