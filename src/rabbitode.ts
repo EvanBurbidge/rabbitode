00e0ceb2ee35f743044a1b1d9b680175ce490d04
eburbidge <eburbidge@hubspot.com>
2021-04-01 11:46:39 +0100

    add all methods to entry

--- src/rabbitode.ts
+++ src/rabbitode.ts
@@ -1,8 +1,17 @@
+import { setDebug } from './logger';
 import { sendMessage } from './publisher';
 import { startConsumer } from './consumers';
 import { getNewChannel } from './channels';
 import { handlePublishError } from './errorHandling';
 import { startRabbit, closeRabbit } from './connection';
+import {
+  getDefaultQueueConfig,
+  getDefaultConsumerConfig, 
+} from './utils';
+import {
+  addToOfflineQueue,
+  getOfflineQueue,
+} from './offline'
 import { decodeToJson, decodeToString, bufferIfy } from './encoding'
 
 export default {
@@ -14,4 +23,10 @@ export default {
   decodeToString,
   bufferIfy,
   getNewChannel,
-}
+  handlePublishError,
+  setDebug,
+  getDefaultConsumerConfig,
+  getDefaultQueueConfig,
+  addToOfflineQueue,
+  getOfflineQueue,
+};
